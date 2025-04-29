9cfe07705064a7378efca14c040224f5e1396813
Drag0nfather <68569666+Drag0nfather@users.noreply.github.com>
2025-04-29 16:18:27 +0300

    Update settings.py

--- parquetework/settings.py
+++ parquetework/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-676)=^_3=1qvqu4d+xkafh$(zn%f1k#9rha+v!=34_%2s+$_ai'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['zona-parketa.ru', 'www.zona-parketa.ru']
 
 
 # Application definition
